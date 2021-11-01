9ecb0433b2acbd02ae79ef3e49f14c7c24d7a63c
yu123an <43343181+yu123an@users.noreply.github.com>
2021-11-01 16:16:13 +0800

    更新TM1650驱动

--- STM_CLOCK.c
+++ STM_CLOCK.c
@@ -12,7 +12,8 @@ uint8_t number[] = {
   //0   1     2     3     4     5     6     7     8     9
   0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F,
   0xBF, 0x86, 0xDB, 0xCF, 0xE6, 0xED, 0xFD, 0x87, 0xFF, 0xEF
-}; void i2c_start() {
+};
+void i2c_start() {
   digitalWrite(scl, 1);
   delayMicroseconds(2);
   digitalWrite(sda, 1);
@@ -32,33 +33,36 @@ void i2c_stop() {
   digitalWrite(sda, 1);
   delayMicroseconds(2);
 }
-void i2c_Write(uint8_t data) {
-  for (int i = 0; i <= 7; i++) {
-    if (data % 2) {
-      digitalWrite(sda, 1);
-      delayMicroseconds(2);
-      digitalWrite(scl, 0);
-      delayMicroseconds(2);
-      digitalWrite(scl, 1);
-      delayMicroseconds(2);
-      digitalWrite(scl, 0);
-      delayMicroseconds(2);
-      digitalWrite(sda, 0);
-    }
-    else
-    {
-      delayMicroseconds(2);
-      digitalWrite(sda, 0);
-      delayMicroseconds(2);
-      digitalWrite(scl, 0);
-      delayMicroseconds(2);
-      digitalWrite(scl, 1);
-      delayMicroseconds(2);
-      digitalWrite(scl, 0);
-      delayMicroseconds(2);
-      digitalWrite(sda, 0);
-    }
-    data /= 2;
+void i2c_ack() {
+  int v = digitalRead(sda);
+  while (v == 1) {
+    delay(10);
+    // Serial_println_s("wite the call back");
+    v = digitalRead(sda);
+  }
+  //Serial_println_s("Got the call back");
+  pinMode(sda, OUTPUT);
+  digitalWrite(scl, 0);
+  delayMicroseconds(2);
+  digitalWrite(sda, 1);
+  delayMicroseconds(2);
+  digitalWrite(scl, 1);
+  delayMicroseconds(2);
+  digitalWrite(scl, 0);
+  delayMicroseconds(2);
+}
+void i2c_write(uint8_t data) {
+  pinMode(sda, OUTPUT);
+  for ( int j = 7; j >= 0; j--) {
+    digitalWrite(sda, (data >> j) & 0x01);
+    delayMicroseconds(1);
+    digitalWrite(scl, 0);
+    delayMicroseconds(1);
+    digitalWrite(scl, 1);
+    delayMicroseconds(1);
+    digitalWrite(scl, 0);
+    delayMicroseconds(1);
+    digitalWrite(sda, 0);
   }
 }
 /*
@@ -190,6 +194,38 @@ void setup() {
 /*
    蜂鸣器
 */
+void dis_num(uint8_t num1,uint8_t num2,uint8_t num3,uint8_t num4){
+   i2c_start();
+    i2c_write(0x48);
+    i2c_ack();
+    i2c_write(0x41);
+    i2c_ack();
+    i2c_stop();
+    i2c_start();
+    i2c_write(0x68);
+    i2c_ack();
+    i2c_write(num1);
+    i2c_ack();
+    i2c_stop();
+    i2c_start();
+    i2c_write(0x6a);
+    i2c_ack();
+    i2c_write(num2);
+    i2c_ack();
+    i2c_stop();
+    i2c_start();
+    i2c_write(0x6c);
+    i2c_ack();
+    i2c_write(num3);
+    i2c_ack();
+    i2c_stop();
+    i2c_start();
+    i2c_write(0x6e);
+    i2c_ack();
+    i2c_write(num4);
+    i2c_ack();
+    i2c_stop();
+}
 void dot() {
   for (int i = 0; i < 20; i++) {
     digitalWrite(BUZ, 1);
@@ -201,26 +237,7 @@ void dot() {
 }
 void loop() {
   get_time();
-  dot();
-  i2c_start();
-  i2c_Write(0x24);
-  i2c_Write(0x41);
-  i2c_stop();
-  i2c_start();
-  i2c_Write(0x34);
-  i2c_Write(number[1]);
-  i2c_stop();
-  i2c_start();
-  i2c_Write(0x35);
-  i2c_Write(number[1]);
-  i2c_stop();
-  i2c_start();
-  i2c_Write(0x36);
-  i2c_Write(number[1]);
-  i2c_stop();
-  i2c_start();
-  i2c_Write(0x37);
-  i2c_Write(number[1]);
-  i2c_stop();
-  delay(2000);
+  //dot();
+  dis_num(number[minute / 10],number[minute % 10],number[second / 10],number[second % 10]);
+  delay(998);
 }
