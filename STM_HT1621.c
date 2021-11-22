9767d0a8efb7735c4d82facd97d8821bd5023d9b
yu123an <43343181+yu123an@users.noreply.github.com>
2021-11-22 16:25:02 +0800

    融合

--- STM_HT1621.c
+++ STM_HT1621.c
@@ -35,7 +35,7 @@
     |   |/    |    \|
     |   +-----D-----+   .
 */
-
+//阵脚引用待调整
 #include <Arduino.h>
 #define cs_HT PD3 //PB4 /*HT1621*/
 //#define clk PA1   //PA3
@@ -66,7 +66,7 @@
 #define TONEOFF 0X08 //0b1000 0001 0000 关闭声音输出
 #define WDTDIS1 0X50 //0b1000 0000 1010  禁止看门狗
 
-#define cs_DS PD3 //PC6 /*DS1302*/
+#define cs_DS PD4 //PC6 /*DS1302*/
 #define clk PA1   //PA3
 #define dat PA2   //PA2
 #define REG_BURST 0xBE
@@ -77,29 +77,24 @@ int CHANGE_NUMER;
 uint8_t Light;
 //数组
 uint8_t REG_FLAG[] = {
-  /*8   C     L     S     F     H     d     y     n*/
-  0xFF, 0xE2, 0xE4, 0x80, 0x82, 0x84, 0x86, 0x88, 0x8C
-};
+    /*8   C     L     S     F     H     d     y     n*/
+    0xFF, 0xE2, 0xE4, 0x80, 0x82, 0x84, 0x86, 0x88, 0x8C};
 uint8_t left_FLAG[] = {
-  /*8   C     L     S     F     H     d     y     n*/
-  0xFF, 0xFF, 0xFF, 0x7F, 0x7F, 0x3F, 0x3F, 0x1F, 0x7F
-};
+    /*8   C     L     S     F     H     d     y     n*/
+    0xFF, 0xFF, 0xFF, 0x7F, 0x7F, 0x3F, 0x3F, 0x1F, 0x7F};
 uint8_t IMG_FLAG[] = {
-  /*8   C     L     S     F     H     d     y     n*/
-  0xFF, 0x39, 0x38, 0x6D, 0x71, 0x76, 0x5E, 0x6E, 0x54
-};
+    /*8   C     L     S     F     H     d     y     n*/
+    0xFF, 0x39, 0x38, 0x6D, 0x71, 0x76, 0x5E, 0x6E, 0x54};
 uint8_t second, minute, hour, day, mouth, dow, year;
 int debug = 1;
 int Deadline = 0;
 uint8_t number[] = {
-  //0   1     2     3     4     5     6     7     8     9
-  0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F,
-  0xBF, 0x86, 0xDB, 0xCF, 0xE6, 0xED, 0xFD, 0x87, 0xFF, 0xEF
-};
+    //0   1     2     3     4     5     6     7     8     9
+    0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F,
+    0xBF, 0x86, 0xDB, 0xCF, 0xE6, 0xED, 0xFD, 0x87, 0xFF, 0xEF};
 
 uint8_t num_DIS[] = {
-  0x7e, 0x12, 0xBC, 0xB6, 0xD2, 0xE5, 0xEE, 0x32, 0xFE, 0xF5
-};
+    0x7e, 0x12, 0xBC, 0xB6, 0xD2, 0xE5, 0xEE, 0x32, 0xFE, 0xF5};
 
 //R&W data
 //统一为从右至左操作
@@ -115,6 +110,18 @@ void Write_data(uint8_t data, uint8_t Long)
     digitalWrite(clk, HIGH);
     delayMicroseconds(2);
     data >>= 1;
+  }
+}
+void Write_addr(uint8_t data, uint8_t Long)
+{
+  for (int i = 0; i < Long; i++)
+  {
+    digitalWrite(clk, LOW);
+    delayMicroseconds(2);
+    digitalWrite(dat, (data & 0x80) ? HIGH : LOW);
+    digitalWrite(clk, HIGH);
+    delayMicroseconds(2);
+    data <<= 1;
   }
 }
 
@@ -223,29 +230,30 @@ void DS1302_INT()
 }
 
 //HT1621  操作
-//待制作
 // 指定地址写数据
 void Write_dataClkData(uint8_t addr, uint8_t data)
 {
-  //addr <<= 2;
+  addr <<= 2;
   digitalWrite(cs_HT, LOW);
   Write_data(0x05, 3);
-  Write_data(addr, 6);
+  Write_addr(addr, 6);
   Write_data(data, 4);
   digitalWrite(cs_HT, HIGH);
 }
 //连续地址写数据
 void Write_dataClkData_(uint8_t addr, uint8_t data)
 {
-  //addr <<= 2;
+  addr <<= 2;
   digitalWrite(cs_HT, LOW);
   Write_data(0x05, 3);
-  Write_data(addr, 6);
-  for (int m = 0; m < 32; m++)
+  Write_addr(addr, 6);
+  for (int m = 0; m < 8; m++)
   {
+    //data由高到低进行取模
     //Write_data(((data >> m) & 0x01) << 3, 4); //待补充
     // Write_data(0xf0 >> (4 * ((data >> (8 - m)) & 0x01)), 4);
-    Write_data(data, 4);
+    Write_data((data & (0x01 << m)) >> m, 4);
+    // Write_data(data, 4);
   }
   digitalWrite(cs_HT, HIGH);
 }
@@ -276,6 +284,146 @@ void HT1621_INT()
   pinMode(dat, OUTPUT);
   HT1621_begin();
 }
+//ADC电量检测
+void ADC_INT()
+{
+  GPIO_Init(GPIOC, GPIO_PIN_4, GPIO_MODE_IN_FL_NO_IT);
+
+  ADC1_DeInit(); //ADC1_CONVERSIONMODE_CONTINUOUS
+
+  ADC1_Init(ADC1_CONVERSIONMODE_SINGLE, //单次转换
+
+            ADC1_CHANNEL_2, //通道
+
+            ADC1_PRESSEL_FCPU_D4, //预定标器选择 分频器  fMASTER 可以被分频 2 到 18
+
+            ADC1_EXTTRIG_TIM, //从内部的TIM1 TRGO事件转换
+
+            DISABLE, //是否使能该触发方式
+
+            ADC1_ALIGN_RIGHT, //对齐方式（可以左右对齐）
+
+            ADC1_SCHMITTTRIG_CHANNEL2, //指定触发通道
+
+            ENABLE); //是否使能指定触发通道
+
+  ADC1_Cmd(ENABLE);
+}
+uint16_t ADC_GET()
+{
+  uint16_t value, temph;
+
+  uint8_t templ;
+
+  ADC1_StartConversion();
+
+  // 定义templ存储低8位数据  temph存储高8位数据
+
+  while (!(ADC1->CSR & 0x80))
+    ; //等待转换完成
+
+  templ = ADC1->DRL;
+
+  temph = ADC1->DRH; //读取ADC转换  在左对齐和右对齐模式下 读取数据的顺序不同  参考STM8寄存器.PDFP371
+
+  value = (templ | (temph << 8)); //注意是10位的转换精度 value、temph应为unsigned int 变量
+  float charing = value * 6.6 * 100 / 1024;
+  return charing;
+}
+int DIS_BAT()
+{
+  int adc = 0;
+  Serial_print_s("The battery is :");
+  for (int nA = 0; nA < 8; nA++)
+  {
+    adc += ADC_GET();
+  }
+  //Serial_println_i(adc >> 3);
+  return adc >> 3;
+}
+
+//中断函数
+void PA3_INT()
+{
+  if (DIS_FLAG == 9)
+  {
+    DIS_FLAG = 0;
+    Deadline = 0;
+  }
+  else
+  {
+    DIS_FLAG += 1;
+  }
+  if (DIS_FLAG == 6)
+  {
+    DIS_FLAG = 0;
+  }
+}
+void PB5_INT()
+{
+
+  DIS_FLAG = 9;
+}
+void PC7_INT()
+{
+  _prepareRead(REG_FLAG[DIS_FLAG]);
+  CHANGE_NUMER = _bcd2dec(Read_data() & left_FLAG[DIS_FLAG]);
+  _end();
+  CHANGE_NUMER += 1;
+  _prepareWrite(REG_WP);
+  Write_data(0b00000000, 8);
+  _end();
+  _prepareWrite(REG_FLAG[DIS_FLAG]);
+  Write_data(_dec2bcd(CHANGE_NUMER), 8);
+  Write_data(0b10000000, 8);
+  _end();
+}
+void PD2_INT()
+{
+  _prepareRead(REG_FLAG[DIS_FLAG]);
+  CHANGE_NUMER = _bcd2dec(Read_data() & left_FLAG[DIS_FLAG]);
+  _end();
+  CHANGE_NUMER -= 1;
+  _prepareWrite(REG_WP);
+  Write_data(0b00000000, 8);
+  _end();
+  _prepareWrite(REG_FLAG[DIS_FLAG]);
+  Write_data(_dec2bcd(CHANGE_NUMER), 8);
+  Write_data(0b10000000, 8);
+  _end();
+}
+//中断使能
+void interrupt_int()
+{
+
+  /*
+     STM INTERRUPT SETTING
+  */
+  //PA3
+  GPIO_Init(GPIOA, GPIO_PIN_3, GPIO_MODE_IN_FL_IT);
+  disableInterrupts();
+  EXTI_SetExtIntSensitivity(EXTI_PORT_GPIOA, FALLING);
+  enableInterrupts();
+  attachInterrupt(INT_PORTA & 0xff, PA3_INT, 0);
+  //PB5
+  GPIO_Init(GPIOB, GPIO_PIN_5, GPIO_MODE_IN_FL_IT);
+  disableInterrupts();
+  EXTI_SetExtIntSensitivity(EXTI_PORT_GPIOB, FALLING);
+  enableInterrupts();
+  attachInterrupt(INT_PORTB & 0xff, PB5_INT, 0);
+  //PC7
+  GPIO_Init(GPIOC, GPIO_PIN_7, GPIO_MODE_IN_FL_IT);
+  disableInterrupts();
+  EXTI_SetExtIntSensitivity(EXTI_PORT_GPIOC, FALLING);
+  enableInterrupts();
+  attachInterrupt(INT_PORTC & 0xff, PC7_INT, 0);
+  //PD2
+  GPIO_Init(GPIOD, GPIO_PIN_2, GPIO_MODE_IN_FL_IT);
+  disableInterrupts();
+  EXTI_SetExtIntSensitivity(EXTI_PORT_GPIOD, FALLING);
+  enableInterrupts();
+  attachInterrupt(INT_PORTD & 0xff, PD2_INT, 0);
+}
 
 void setup()
 {
@@ -286,9 +434,9 @@ void setup()
 
 void loop()
 {
-  Write_dataClkData_(0, 0xff);
+  Write_dataClkData_(0x00, 0x3F);
   delay(2000);
-  Write_dataClkData_(0, 0x00);
+  Write_dataClkData_(0x00, 0x00);
   delay(2000);
   get_time();
   HT1621_INT();
