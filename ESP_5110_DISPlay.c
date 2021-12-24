29552301aa44072c002024503158ae70dacbaf41
yu123an <43343181+yu123an@users.noreply.github.com>
2021-12-25 01:06:40 +0800

    Update ESP_5110_DISPlay.c

--- ESP_5110_DISPlay.c
+++ ESP_5110_DISPlay.c
@@ -1,445 +1,267 @@
-/**************************************************************************/
-/*!
-  @file Adafruit_PCD8544.cpp
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
+//宏定义
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+#define LOGO16_GLCD_HEIGHT 16
+#define LOGO16_GLCD_WIDTH 16
+//引脚声明
+#define DIS_CLK 0 
+#define DIS_SDA 2 
+#define DIS_DC 4 
+#define DIS_CE 5 
+#define DIS_RST 16 
+#define RTC_SDA 2 
+#define RTC_SCL 0 
+#define RTC_CE 5 
+#define BUZ 15 
+#define KEY_1 14 
+#define KEY_2 13 
+#define KEY_3 12 
+#define debug 1
+#define REG_BURST 0xBE
+#define REG_WP 0x8E
+uint8_t second, minute, hour, day, mouth, dow, year;
+Adafruit_PCD8544 display = Adafruit_PCD8544(DIS_CLK, DIS_SDA, DIS_DC, DIS_CE, DIS_RST);
+void testdrawchar(void) {
+  display.setTextSize(1);
+  display.setTextColor(BLACK);
+  display.setCursor(0,0);
 
-  @mainpage Adafruit PCD8544 Nokia 5110 LCD Library
-
-  @section intro Introduction
-
-  This is a library for our Monochrome Nokia 5110 LCD Displays
-
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/products/338
-
-  These displays use SPI to communicate, 4 or 5 pins are required to
-  interface
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  @section author Author
-
-  Written by Limor Fried/Ladyada  for Adafruit Industries.
-
-  @section license License
-
-  BSD license, check license.txt for more information
-  All text above, and the splash screen below must be included in any
-  redistribution
-
- */
-/**************************************************************************/
-
-#include "Adafruit_PCD8544.h"
-#include "Arduino.h"
-#include <stdlib.h>
-
-/** the memory buffer for the LCD */
-uint8_t pcd8544_buffer[LCDWIDTH * LCDHEIGHT / 8] = {
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0xC0, 0xE0, 0xF0, 0xF8, 0xFC, 0xFC, 0xFE, 0xFF, 0xFC, 0xE0,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0xF8, 0xF8, 0xF8, 0xF8, 0xF8, 0xF8, 0xF8,
-    0xF8, 0xF8, 0xF8, 0xF8, 0xF8, 0xF0, 0xF0, 0xE0, 0xE0, 0xC0, 0x80, 0xC0,
-    0xFC, 0xFF, 0xFF, 0xFF, 0xFF, 0x7F, 0x3F, 0x7F, 0xFF, 0xFF, 0xFF, 0xFF,
-    0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0F, 0x1F, 0x3F, 0x7F, 0xFF, 0xFF,
-    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xE7, 0xC7, 0xC7, 0x87, 0x8F, 0x9F,
-    0x9F, 0xFF, 0xFF, 0xFF, 0xC1, 0xC0, 0xE0, 0xFC, 0xFF, 0xFF, 0xFF, 0xFF,
-    0xFF, 0xFC, 0xFC, 0xFC, 0xFC, 0xFE, 0xFE, 0xFE, 0xFC, 0xFC, 0xF8, 0xF8,
-    0xF0, 0xE0, 0xC0, 0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 0xC0, 0xE0,
-    0xF1, 0xFB, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x7F, 0x1F, 0x0F, 0x0F, 0x87,
-    0xE7, 0xFF, 0xFF, 0xFF, 0x1F, 0x1F, 0x3F, 0xF9, 0xF8, 0xF8, 0xF8, 0xF8,
-    0xF8, 0xF8, 0xFD, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-    0x7F, 0x3F, 0x0F, 0x07, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xF0, 0xFE, 0xFF, 0xFF, 0xFF,
-    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFE, 0x7E, 0x3F, 0x3F, 0x0F,
-    0x1F, 0xFF, 0xFF, 0xFF, 0xFC, 0xF0, 0xE0, 0xF1, 0xFF, 0xFF, 0xFF, 0xFF,
-    0xFF, 0xFC, 0xF0, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x03, 0x03, 0x03, 0x03,
-    0x03, 0x03, 0x03, 0x01, 0x01, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x03, 0x0F, 0x1F, 0x3F, 0x7F, 0x7F, 0xFF, 0xFF, 0xFF, 0xFF,
-    0x7F, 0x7F, 0x1F, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-};
-
-/*!
-  @brief Update the bounding box for partial updates
-  @param xmin left
-  @param ymin bottom
-  @param xmax right
-  @param ymax top
- */
-void Adafruit_PCD8544::updateBoundingBox(uint8_t xmin, uint8_t ymin,
-                                         uint8_t xmax, uint8_t ymax) {
-  xUpdateMin = min(xUpdateMin, xmin);
-  xUpdateMax = max(xUpdateMax, xmax);
-  yUpdateMin = min(yUpdateMin, ymin);
-  yUpdateMax = max(yUpdateMax, ymax);
+  for (uint8_t i=0; i < 168; i++) {
+    if (i == '\n') continue;
+    display.write(i);
+    //if ((i > 0) && (i % 14 == 0))
+      //display.println();
+  }    
+  display.display();
 }
 
-/*!
-  @brief Constructor for software SPI with explicit CS pin
-  @param sclk_pin SCLK pin
-  @param din_pin  DIN pin
-  @param dc_pin   DC pin
-  @param cs_pin   CS pin
-  @param rst_pin  RST pin
- */
-Adafruit_PCD8544::Adafruit_PCD8544(int8_t sclk_pin, int8_t din_pin,
-                                   int8_t dc_pin, int8_t cs_pin, int8_t rst_pin)
-    : Adafruit_GFX(LCDWIDTH, LCDHEIGHT) {
-  spi_dev = new Adafruit_SPIDevice(cs_pin, sclk_pin, -1, din_pin,
-                                   4000000); // 4mhz max speed
-
-  _dcpin = dc_pin;
-  _rstpin = rst_pin;
-}
-
-/*!
-  @brief Constructor for hardware SPI based on hardware controlled SCK (SCLK)
-  and MOSI (DIN) pins. CS is still controlled by any IO pin. NOTE: MISO and SS
-  will be set as an input and output respectively, so be careful sharing those
-  pins!
-  @param dc_pin   DC pin
-  @param cs_pin   CS pin
-  @param rst_pin  RST pin
-  @param theSPI   Pointer to SPIClass device for hardware SPI
- */
-Adafruit_PCD8544::Adafruit_PCD8544(int8_t dc_pin, int8_t cs_pin, int8_t rst_pin,
-                                   SPIClass *theSPI)
-    : Adafruit_GFX(LCDWIDTH, LCDHEIGHT) {
-  spi_dev = new Adafruit_SPIDevice(cs_pin, 4000000, SPI_BITORDER_MSBFIRST,
-                                   SPI_MODE0, theSPI);
-
-  _dcpin = dc_pin;
-  _rstpin = rst_pin;
-}
-
-/*!
-  @brief The most basic function, set a single pixel, in the main buffer
-  @param x     x coord
-  @param y     y coord
-  @param color pixel color (BLACK or WHITE)
- */
-void Adafruit_PCD8544::drawPixel(int16_t x, int16_t y, uint16_t color) {
-  setPixel(x, y, color, pcd8544_buffer);
-}
-
-/*!
-  @brief The most basic function, set a single pixel
-  @param x     x coord
-  @param y     y coord
-  @param color pixel color (BLACK or WHITE)
-  @param buffer The framebuffer to set the pixel in
- */
-void Adafruit_PCD8544::setPixel(int16_t x, int16_t y, bool color,
-                                uint8_t *buffer) {
-  if ((x < 0) || (x >= _width) || (y < 0) || (y >= _height))
-    return;
-
-  int16_t t;
-  switch (rotation) {
-  case 1:
-    t = x;
-    x = y;
-    y = LCDHEIGHT - 1 - t;
-    break;
-  case 2:
-    x = LCDWIDTH - 1 - x;
-    y = LCDHEIGHT - 1 - y;
-    break;
-  case 3:
-    t = x;
-    x = LCDWIDTH - 1 - y;
-    y = t;
-    break;
+//以下为DS1302操作函数
+void Write_data(uint8_t data)
+{
+  for (int i = 0; i < 8; i++)
+  {
+    digitalWrite(RTC_SCL, LOW);
+    delayMicroseconds(2);
+    digitalWrite(RTC_SDA, (data & 0x01) ? HIGH : LOW);
+    digitalWrite(RTC_SCL, HIGH);
+    delayMicroseconds(2);
+    data >>= 1;
   }
-  updateBoundingBox(x, y, x, y);
-
-  // x is which column
-  if (color)
-    buffer[x + (y / 8) * LCDWIDTH] |= 1 << (y % 8);
-  else
-    buffer[x + (y / 8) * LCDWIDTH] &= ~(1 << (y % 8));
 }
+uint8_t Read_data()
+{
+  uint8_t byte = 0;
 
-/*!
-  @brief The most basic function, get a single pixel
-  @param  x x coord
-  @param  y y coord
-  @param buffer The framebuffer to get the pixel from
-  @return   color of the pixel at x,y
- */
-bool Adafruit_PCD8544::getPixel(int16_t x, int16_t y, uint8_t *buffer) {
-  if ((x < 0) || (x >= _width) || (y < 0) || (y >= _height))
-    return false;
-
-  int16_t t;
-  switch (rotation) {
-  case 1:
-    t = x;
-    x = y;
-    y = LCDHEIGHT - 1 - t;
-    break;
-  case 2:
-    x = LCDWIDTH - 1 - x;
-    y = LCDHEIGHT - 1 - y;
-    break;
-  case 3:
-    t = x;
-    x = LCDWIDTH - 1 - y;
-    y = t;
-    break;
+  for (uint8_t b = 0; b < 8; b++)
+  {
+    digitalWrite(RTC_SCL, LOW);
+    delayMicroseconds(2);
+    if (digitalRead(RTC_SDA) == HIGH)
+      byte |= 0x01 << b;
+    digitalWrite(RTC_SCL, HIGH);
+    delayMicroseconds(2);
   }
-
-  return (buffer[x + (y / 8) * LCDWIDTH] >> (y % 8)) & 0x1;
+  return byte;
 }
-
-/*!
-  @brief Initialize the display. Set bias and contrast, enter normal mode.
- */
-void Adafruit_PCD8544::initDisplay() {
-/*
-此处为5110重置部分，未达到效果，注释掉
-*/
-/*
-  // toggle RST low to reset
-  if (_rstpin >= 0) {
-    pinMode(_rstpin, OUTPUT);
-    digitalWrite(_rstpin, LOW);
-    delay(1); // 1 ns minimum
-    digitalWrite(_rstpin, HIGH);
+void _setDirection(int direction)
+{
+  pinMode(RTC_SDA, direction);
+}
+void _prepareRead(uint8_t address)
+{
+  _setDirection(OUTPUT);
+  digitalWrite(RTC_CE , HIGH);
+  //digitalWrite(cs_HT, HIGH);
+  uint8_t command = 0b10000001 | address;
+  Write_data(command);
+  _setDirection(INPUT);
+}
+void _prepareWrite(uint8_t address)
+{
+  _setDirection(OUTPUT);
+  digitalWrite(RTC_CE , HIGH);
+  //digitalWrite(cs_HT, HIGH);
+  uint8_t command = 0b10000000 | address;
+  Write_data(command);
+}
+void _end()
+{
+  digitalWrite(RTC_CE , LOW);
+  pinMode(RTC_SDA, OUTPUT);
+}
+uint8_t _dec2bcd(uint8_t dec)
+{
+  return ((dec / 10 * 16) + (dec % 10));
+}
+uint8_t _bcd2dec(uint8_t bcd)
+{
+  return ((bcd / 16 * 10) + (bcd % 16));
+}
+void get_time()
+{
+  _prepareRead(REG_BURST);
+  second = _bcd2dec(Read_data() & 0b01111111);
+  minute = _bcd2dec(Read_data() & 0b01111111);
+  hour = _bcd2dec(Read_data() & 0b00111111);
+  day = _bcd2dec(Read_data() & 0b00111111);
+  mouth = _bcd2dec(Read_data() & 0b00011111);
+  dow = _bcd2dec(Read_data() & 0b00000111);
+  year = _bcd2dec(Read_data() & 0b01111111);
+  _end();
+  if (debug)
+  {
+    Serial.print("Now time is ");
+    Serial.print(year);
+    Serial.print("-");
+    Serial.print(mouth);
+    Serial.print("-");
+    Serial.print(day);
+    Serial.print("-");
+    Serial.print(hour);
+    Serial.print(":");
+    Serial.print(minute);
+    Serial.print(":");
+    Serial.println(second);
   }
-*/
-/*
-此处为5110重置部分，未达到效果，注释掉
-*/
-  setBias(_bias);
-  setContrast(_contrast);
-
-  // normal mode
-  command(PCD8544_FUNCTIONSET);
-
-  // Set display to Normal
-  command(PCD8544_DISPLAYCONTROL | PCD8544_DISPLAYNORMAL);
+}
+void set_time(int sec, int minute, int hour, int day, int mouth, int dow, int year)
+{
+  _prepareWrite(REG_WP);
+  Write_data(0b00000000);
+  _end();
+  _prepareWrite(REG_BURST);
+  Write_data(_dec2bcd(sec % 60));
+  Write_data(_dec2bcd(minute % 60));
+  Write_data(_dec2bcd(hour % 24));
+  Write_data(_dec2bcd(day % 32));
+  Write_data(_dec2bcd(mouth % 13));
+  Write_data(_dec2bcd(dow % 8));
+  Write_data(_dec2bcd(year % 100));
+  Write_data(0b10000000);
+  _end();
+}
+void DS1302_INT()
+{
+  pinMode(RTC_CE , OUTPUT);
+  pinMode(RTC_SCL, OUTPUT);
+  pinMode(RTC_SDA, INPUT);
+  digitalWrite(RTC_CE , LOW);
+  digitalWrite(RTC_SCL, LOW);
 }
 
-/*!
-  @brief Set up SPI, initialize the display, set the bounding box
-  @param contrast Initial contrast value
-  @param bias     Initial bias value
-  @returns True on initialization success
- */
-bool Adafruit_PCD8544::begin(uint8_t contrast, uint8_t bias) {
-
-  if (!spi_dev->begin()) {
-    return false;
-  }
-
-  // Set common pin outputs.
-  pinMode(_dcpin, OUTPUT);
-  /*
-此处为5110重置部分，未达到效果，注释掉
-*/
-/*
-  if (_rstpin >= 0)
-    pinMode(_rstpin, OUTPUT);
-*/
-/*
-此处为5110重置部分，未达到效果，注释掉
-*/
-  _bias = bias;
-  _contrast = contrast;
-  _reinit_interval = 0;
-  _display_count = 0;
-  initDisplay();
-
-  // initial display line
-  // set page address
-  // set column address
-  // write display data
-
-  // set up a bounding box for screen updates
-
-  updateBoundingBox(0, 0, LCDWIDTH - 1, LCDHEIGHT - 1);
-  // Push out pcd8544_buffer to the Display (will show the AFI logo)
-  display();
-
-  return true;
-}
-
-/*!
-  @brief  Send a command to the LCD
-  @param c Command byte
- */
-void Adafruit_PCD8544::command(uint8_t c) {
-  digitalWrite(_dcpin, LOW);
-  spi_dev->write(&c, 1);
-}
-
-/*!
-  @brief  Send data to the LCD
-  @param c Data byte
- */
-void Adafruit_PCD8544::data(uint8_t c) {
-  digitalWrite(_dcpin, HIGH);
-  spi_dev->write(&c, 1);
-}
-
-/*!
-  @brief  Set the contrast level
-  @param val Contrast value
- */
-void Adafruit_PCD8544::setContrast(uint8_t val) {
-  if (val > 0x7f) {
-    val = 0x7f;
-  }
-  _contrast = val;
-  command(PCD8544_FUNCTIONSET | PCD8544_EXTENDEDINSTRUCTION);
-  command(PCD8544_SETVOP | val);
-  command(PCD8544_FUNCTIONSET);
-}
-
-/*!
-  @brief  Set the bias level
-  @param val Bias value
- */
-void Adafruit_PCD8544::setBias(uint8_t val) {
-  if (val > 0x07) {
-    val = 0x07;
-  }
-  _bias = val;
-  command(PCD8544_FUNCTIONSET | PCD8544_EXTENDEDINSTRUCTION);
-  command(PCD8544_SETBIAS | val);
-  command(PCD8544_FUNCTIONSET);
-}
-
-/*!
-  @brief  Get the bias level
-  @return Bias value
- */
-uint8_t Adafruit_PCD8544::getBias() { return _bias; }
-
-/*!
-  @brief  Get the contrast level
-  @return Contrast value
- */
-uint8_t Adafruit_PCD8544::getContrast() { return _contrast; }
-
-/*!
-  @brief  Set the interval for reinitializing the display
-  @param val Reinit after this many calls to display()
- */
-void Adafruit_PCD8544::setReinitInterval(uint8_t val) {
-  _reinit_interval = val;
-}
-
-/*!
-  @brief  Get the reinit interval
-  @return Reinit interval
- */
-uint8_t Adafruit_PCD8544::getReinitInterval() { return _reinit_interval; }
-
-/*!
-  @brief Update the display
- */
-void Adafruit_PCD8544::display(void) {
-  if (_reinit_interval) {
-    _display_count++;
-    if (_display_count >= _reinit_interval) {
-      _display_count = 0;
-      initDisplay();
+void buzzer()
+{
+  for (int t = 0; t <2; t++)
+  {
+    digitalWrite(BUZ, 1);
+     for(int i = 0;i<10;i++){
+      delayMicroseconds(1000);
+    }
+    digitalWrite(BUZ, 0);
+    for(int i = 0;i<30;i++){
+      delayMicroseconds(1000);
     }
   }
-
-  for (uint8_t page = (yUpdateMin / 8); page < (yUpdateMax / 8) + 1; page++) {
-    command(PCD8544_SETYADDR | page);
-
-    uint8_t startcol = xUpdateMin;
-    uint8_t endcol = xUpdateMax;
-
-    command(PCD8544_SETXADDR | startcol);
-
-    digitalWrite(_dcpin, HIGH);
-    spi_dev->write(pcd8544_buffer + (LCDWIDTH * page) + startcol,
-                   endcol - startcol + 1);
-  }
-
-  command(PCD8544_SETYADDR); // no idea why this is necessary but it is to
-                             // finish the last byte?
-
-  xUpdateMin = LCDWIDTH - 1;
-  xUpdateMax = 0;
-  yUpdateMin = LCDHEIGHT - 1;
-  yUpdateMax = 0;
 }
-
-/*!
-  @brief Clear the entire display
- */
-void Adafruit_PCD8544::clearDisplay(void) {
-  memset(pcd8544_buffer, 0, LCDWIDTH * LCDHEIGHT / 8);
-  updateBoundingBox(0, 0, LCDWIDTH - 1, LCDHEIGHT - 1);
-  cursor_y = cursor_x = 0;
+ICACHE_RAM_ATTR void KEY_O()
+{
+  Serial.println("The button Key_1 has been press !!!");
+  buzzer();
 }
-
-/*!
-  @brief Invert the entire display
-  @param i True to invert the display, false to keep it uninverted
- */
-void Adafruit_PCD8544::invertDisplay(bool i) {
-  command(PCD8544_FUNCTIONSET);
-  command(PCD8544_DISPLAYCONTROL |
-          (i ? PCD8544_DISPLAYINVERTED : PCD8544_DISPLAYNORMAL));
-}
-
-/*!
-  @brief Scroll the display by creating a new buffer and moving each pixel
-  @param xpixels The x offset, can be negative to scroll backwards
-  @param ypixels The y offset, can be negative to scroll updwards
- */
-void Adafruit_PCD8544::scroll(int8_t xpixels, int8_t ypixels) {
-  uint8_t new_buffer[LCDWIDTH * LCDHEIGHT / 8];
-  memset(new_buffer, 0, LCDWIDTH * LCDHEIGHT / 8);
-
-  // negative pixels wrap around
-  while (ypixels < 0) {
-    ypixels += height();
-  }
-  ypixels %= height();
-  while (xpixels < 0) {
-    xpixels += width();
-  }
-  xpixels %= width();
-
-  for (int x = 0; x < width(); x++) {
-    for (int y = 0; y < height(); y++) {
-      if (getPixel(x, y, pcd8544_buffer)) {
-        int new_x = (x + xpixels) % _width;
-        int new_y = (y + ypixels) % _height;
-        setPixel(new_x, new_y, true, new_buffer);
-      }
+ICACHE_RAM_ATTR void KEY_T()
+{
+  for(int i = 0;i<100;i++){
+      delayMicroseconds(1000);
     }
+  Serial.println("The button Key_2 has been press !!!");
+}
+ICACHE_RAM_ATTR void KEY_TH()
+{
+  for(int i = 0;i<100;i++){
+      delayMicroseconds(1000);
+    }
+  Serial.println("The button Key_3 has been press !!!");
+}
+void testdrawline() {  
+  for (int16_t i=0; i<display.width(); i+=4) {
+    display.drawLine(0, 0, i, display.height()-1, BLACK);
+    display.display();
   }
-  memcpy(pcd8544_buffer, new_buffer, LCDWIDTH * LCDHEIGHT / 8);
-  updateBoundingBox(0, 0, LCDWIDTH - 1, LCDHEIGHT - 1);
+  for (int16_t i=0; i<display.height(); i+=4) {
+    display.drawLine(0, 0, display.width()-1, i, BLACK);
+    display.display();
+  }
+  delay(250);
+  
+  display.clearDisplay();
+  for (int16_t i=0; i<display.width(); i+=4) {
+    display.drawLine(0, display.height()-1, i, 0, BLACK);
+    display.display();
+  }
+  for (int8_t i=display.height()-1; i>=0; i-=4) {
+    display.drawLine(0, display.height()-1, display.width()-1, i, BLACK);
+    display.display();
+  }
+  delay(250);
+  
+  display.clearDisplay();
+  for (int16_t i=display.width()-1; i>=0; i-=4) {
+    display.drawLine(display.width()-1, display.height()-1, i, 0, BLACK);
+    display.display();
+  }
+  for (int16_t i=display.height()-1; i>=0; i-=4) {
+    display.drawLine(display.width()-1, display.height()-1, 0, i, BLACK);
+    display.display();
+  }
+  delay(250);
+
+  display.clearDisplay();
+  for (int16_t i=0; i<display.height(); i+=4) {
+    display.drawLine(display.width()-1, 0, 0, i, BLACK);
+    display.display();
+  }
+  for (int16_t i=0; i<display.width(); i+=4) {
+    display.drawLine(display.width()-1, 0, i, display.height()-1, BLACK); 
+    display.display();
+  }
+  delay(250);
+}
+void setup()
+{
+  display.begin();
+  DS1302_INT();
+  Serial.begin(9600);
+  pinMode(KEY_1, INPUT_PULLUP);
+  pinMode(KEY_2, INPUT_PULLUP);
+  pinMode(KEY_3, INPUT_PULLUP);
+  pinMode(BUZ, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(KEY_1), KEY_O, FALLING);
+  attachInterrupt(digitalPinToInterrupt(KEY_2), KEY_T, FALLING);
+  attachInterrupt(digitalPinToInterrupt(KEY_3), KEY_TH, FALLING);
+}
+void loop()
+{
+  display.setContrast(75);
+
+  display.display(); // show splashscreen
+  delay(2000);
+  display.clearDisplay();   // clears the screen and buffer
+
+  // draw a single pixel
+  display.drawPixel(10, 10, BLACK);
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  // draw many lines
+  testdrawline();
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+  get_time();
 }
