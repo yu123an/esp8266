241a1bfca2c8063baa5d1f098e6a600b9bb3ebb8
yu123an <43343181+yu123an@users.noreply.github.com>
2022-09-19 12:29:41 +0800

    Update ESP8266_light.ino
    增加注释

--- EPS32/ESP8266_light.ino
+++ EPS32/ESP8266_light.ino
@@ -7,9 +7,14 @@
 #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
 //引脚定义
+//双色温LED对应PWM引脚
 #define WhiteLed 5
 #define YellowLed 4
+//联网设置引脚
 #define WiFi_State 4
+//联网标志位
+int WiFiFlag;
+// WS2812控制引脚，灯珠数
 #define PIN 15
 #define NUMPIXELS 11
 //编码器引脚
@@ -17,20 +22,27 @@
 #define ROTARY_ENCODER_B_PIN 14
 #define ROTARY_ENCODER_BUTTON_PIN 13
 #define ROTARY_ENCODER_VCC_PIN -1
+//未知参数
 #define ROTARY_ENCODER_STEPS 4
 //变量声明
-int ButtonFlag = 1;
+int ButtonFlag = 1; //按钮功能标志位；1调整亮度；0调整色温
+//亮度值
 int LedBrightness = 128;
+//双灯珠色温
 int ColorTempWhite = 127;
 int ColorTempYellow = 128;
+//双灯珠亮度
 int WhiteBrightness = 127;
 int YellowBrightness = 128;
+//亮度，色温记忆功能
 int LastLedBrightness;
 int LastColorTempWhite;
-//联网标志位
-int WiFiFlag;
 //实例对象
-AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, ROTARY_ENCODER_VCC_PIN, ROTARY_ENCODER_STEPS);
+AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(ROTARY_ENCODER_A_PIN,
+														  ROTARY_ENCODER_B_PIN,
+														  ROTARY_ENCODER_BUTTON_PIN,
+														  ROTARY_ENCODER_VCC_PIN,
+														  ROTARY_ENCODER_STEPS);
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 Ticker ticker;
 void IRAM_ATTR readEncoderISR()
@@ -45,6 +57,8 @@ void setup()
 #endif
 	pixels.begin();
 	pinMode(WiFi_State, INPUT);
+	pinMode(WhiteLed, OUTPUT);
+	pinMode(YellowLed, OUTPUT);
 	WiFiFlag = digitalRead(WiFi_State);
 	if (WiFiFlag)
 	{
@@ -62,10 +76,14 @@ void setup()
 	bool circleValues = false;
 	rotaryEncoder.setBoundaries(0, 255, circleValues); // minValue, maxValue, circleValues true|false (when max go to min and vice versa)
 	rotaryEncoder.setAcceleration(250);				   // or set the value - larger number = more accelearation; 0 or 1 means disabled acceleration
-	analogWriteFreq(10000);							   // 10KHZ
+	// pwm频率
+	analogWriteFreq(10000); // 10KHZ
+	//设置编码器初始值
 	rotaryEncoder.setEncoderValue(127);
+	//设置亮度值
 	analogWrite(WhiteLed, 0);
 	analogWrite(YellowLed, 0);
+	// ec11检测
 	ticker.attach_ms(500, rotary_loop);
 }
 
@@ -86,6 +104,8 @@ void rotary_onButtonClick()
 	Serial.print("button pressed ");
 	Serial.print(millis());
 	Serial.println(" milliseconds after restart");
+	//循环设置亮度与色温值
+	//读取亮度，色温记忆值
 	ButtonFlag -= 1;
 	if (ButtonFlag)
 	{
@@ -99,6 +119,9 @@ void rotary_onButtonClick()
 }
 void rotary_loop()
 {
+	//先行调解亮度和色温，
+	//单灯珠亮度等于总亮度*色温÷256
+	//最终得出每种色温灯珠的亮度值
 	// dont print anything unless value changed
 	if (rotaryEncoder.encoderChanged())
 	{
