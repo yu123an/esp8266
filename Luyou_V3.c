c9f5a3b46ff0717fa9047662f60ae945e072a53d
yu123an <43343181+yu123an@users.noreply.github.com>
2022-09-21 14:11:48 +0800

    Update Luyou_V3.c

--- Luyou_V3.c
+++ Luyou_V3.c
@@ -1,4 +1,4 @@
-#include <StackThunk.h>
+//加载库
 #include <ArduinoJson.h>
 #include <Wire.h>
 #include <ESP8266WiFi.h>
@@ -10,68 +10,26 @@
 #include <EEPROM.h>
 #include <ThreeWire.h> //DS1302
 #include <RtcDS1302.h>
-//以下为MQtt通讯部分代码
-#include "Adafruit_MQTT.h"
-#include "Adafruit_MQTT_Client.h"
 #include <PubSubClient.h>
-
-/*
-  #define AIO_SERVER "*********"
-  #define AIO_SERVERPORT ********* // use 8883 for SSL
-  #define AIO_USERNAME "*********" //百度云MQTT用户名
-  #define AIO_KEY "*********"     //百度云MQTT用户密码
-*/
+//敏感信息
+const char *mqtt_server = "*********";
+const char *ssid = "wei xing ban ban gong shi";
+const char *password = "weixing1234+-*/";
+//LED使能引脚
 #define LED 13
+//LOG打印标志位
 #define debug 1
-WiFiClient client;
-WiFiUDP ntpUDP;
-// MQTT客户端链接
-Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
-// MQTT消息订阅与发布
-//  Setup a feed called 'onoff' for subscribing to changes to the button
-Adafruit_MQTT_Subscribe get_msg = Adafruit_MQTT_Subscribe(&mqtt, "EspIn", MQTT_QOS_1);
-Adafruit_MQTT_Publish Pub_Temp = Adafruit_MQTT_Publish(&mqtt, "EspOut");
+//实例化类
+WiFiClient espClient;
+PubSubClient client(espClient);
 ThreeWire myWire(15, 0, 14); // IO, SCLK, CE
 RtcDS1302<ThreeWire> Rtc(myWire);
 Ticker update_msg;
-uint32_t x = 0;
-const char *ssid = "wei xing ban ban gong shi";
-const char *password = "weixing1234+-*/";
-StaticJsonDocument<400> doc;
-StaticJsonDocument<400> web;
 StaticJsonDocument<800> Mqtt_Sub;
+//变量声明
 int Sun_rise_hour, Sun_rise_minute, Sun_set_hour, Sun_set_minute;
 int _hour, _minute;
-// MQTT心跳包检测
-void MQTT_connect()
-{
-  int8_t ret;
-
-  // Stop if already connected.
-  if (mqtt.connected())
-  {
-    return;
-  }
-
-  Serial.print("Connecting to MQTT... ");
-
-  uint8_t retries = 3;
-  while ((ret = mqtt.connect()) != 0)
-  { // connect will return 0 for connected
-    Serial.println(mqtt.connectErrorString(ret));
-    Serial.println("Retrying MQTT connection in 10 seconds...");
-    mqtt.disconnect();
-    delay(10000); // wait 10 seconds
-    retries--;
-    if (retries == 0)
-    {
-      // basically die and wait for WDT to reset me
-      while (1)
-        ;
-    }
-  }
-  Serial.println("MQTT Connected!");
-}
+//读取温度
 int get_temp()
 {
   Wire.begin(5, 4);
@@ -86,67 +44,7 @@ int get_temp()
   }
   return temp;
 }
-void Get_Mqtt(uint32_t x)
-{
-  MQTT_connect();
-  Adafruit_MQTT_Subscribe *subscription;
-  while ((subscription = mqtt.readSubscription(800)))
-  {
-    if (debug)
-    {
-      // Serial.println("The Sub Topic is :");
-      // Serial.println(*subcription);
-      Serial.println("I get th msg :");
-      Serial.println((char *)get_msg.lastread);
-    }
-    //处理接收到的信息,进行Json化
-    deserializeJson(Mqtt_Sub, (char *)get_msg.lastread);
-    String type = Mqtt_Sub["Type"];
-    if (type == "time")
-    {
-      Serial.println("准备更新时间");
-      int _hour = Mqtt_Sub["hour"];
-      int _minute = Mqtt_Sub["minute"];
-      int _second = Mqtt_Sub["second"];
-      Rtc.My_SetDateTime(_second, _minute, _hour, 2, 2, 2, 22);
-    }
-    else if (type == "SunTime")
-    {
-      Sun_rise_hour = Mqtt_Sub["R_H"];
-      Sun_rise_minute = Mqtt_Sub["R_M"];
-      Sun_set_hour = Mqtt_Sub["S_H"];
-      Sun_set_minute = Mqtt_Sub["S_M"];
-      EEPROM.write(0, Sun_rise_hour);
-      EEPROM.write(1, Sun_rise_minute);
-      EEPROM.write(2, Sun_set_hour);
-      EEPROM.write(3, Sun_set_minute);
-      EEPROM.commit();
-    }
-    else if (type == "Light")
-    {
-      Serial.println("Ready to open led");
-      if (Mqtt_Sub["light"] == 1)
-      {
-        digitalWrite(LED, 1);
-        Serial.println("open led");
-      }
-      else
-      {
-        digitalWrite(LED, 0);
-        Serial.println("close led");
-      }
-    }
-    else
-    {
-      Serial.println("Nothing");
-    }
-  }
-  //保持连接
-  if (!mqtt.ping())
-  {
-    mqtt.disconnect();
-  }
-}
+//联网
 void Get_Net()
 {
   WiFi.begin(ssid, password);
@@ -160,11 +58,11 @@ void Get_Net()
 void setup()
 {
   Serial.begin(9600);
+  //申请EEPROM读写空间
   EEPROM.begin(64);
+  //联网
   Get_Net();
-  get_msg.setCallback(Get_Mqtt);
-  mqtt.subscribe(&get_msg);
-  MQTT_connect();
+  //操作DS1302
   Rtc.Begin();
   Rtc.SetIsRunning(true);
   //解除写保护
@@ -173,45 +71,118 @@ void setup()
     Serial.println("RTC was write protected, enabling writing now");
     Rtc.SetIsWriteProtected(false);
   }
-  update_msg.attach(5,out_msg);
+  //定时任务，每隔5s发布信息
+  update_msg.attach(5, Pub_msg);
+  //MQTT，指定服务器，回调函数，重连操作
+  client.setServer(mqtt_server, 1383);
+  client.setCallback(callback);
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  client.loop();
 }
 void loop()
 {
-  RtcDateTime now = Rtc.GetDateTime();
-  if ((now.Hour() == Sun_rise_hour) && (now.Minute() == Sun_rise_minute))
+   //MQTT心跳检测
+  if (!client.connected())
   {
-    Serial.println("准备开灯");
-    digitalWrite(LED, 1);
-    for (int i = 0; i < 61; i++)
-    {
-      delay(1000);
-    }
+    reconnect();
   }
-  else if ((now.Hour() == Sun_set_hour) && (now.Minute() == Sun_set_minute))
-  {
-    Serial.println("准备关灯");
-    digitalWrite(LED, 0);
-    for (int i = 0; i < 61; i++)
-    {
-      delay(1000);
-    }
-  }
-  else
-  {
-    Serial.println("Nothing happen");
-  }
-  Serial.println("I am ok !!!");
-  MQTT_connect();
-  // mqtt.subscribe(&get_msg);
-  Get_Mqtt(3);
+  client.loop();
   delay(998);
   get_temp();
 }
-void out_msg(){
-  String pubA = "{\"Temp\":";
-    String pubB = ",\"light\":";
-    String pub2 = pubA + get_temp() + pubB + digitalRead(LED) + "}";
-    char pubb[200];
-    pub2.toCharArray(pubb, 200);
-    Pub_Temp.publish(pubb);
+// MQTT相关函数
+//订阅主题：EspIn
+//发布主题：EspOut
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  payload[length] = 0;
+  deserializeJson(Mqtt_Sub, String((char *)payload)); //对接收到的MQTT_Message进行JSON解析
+  // JSON文件格式：https://github.com/yu123an/esp8266/blob/master/EPS32/json.log
+  //"Type": "weather",//消息类型，包含：weather，message，gif，
+  String type = Mqtt_Sub["Type"];
+  if (type == "time")
+  {
+    Serial.println("准备更新时间");
+    int _hour = Mqtt_Sub["hour"];
+    int _minute = Mqtt_Sub["minute"];
+    int _second = Mqtt_Sub["second"];
+    Rtc.My_SetDateTime(_second, _minute, _hour, 2, 2, 2, 22);
+  }
+  else if (type == "SunTime")
+  {
+    Sun_rise_hour = Mqtt_Sub["R_H"];
+    Sun_rise_minute = Mqtt_Sub["R_M"];
+    Sun_set_hour = Mqtt_Sub["S_H"];
+    Sun_set_minute = Mqtt_Sub["S_M"];
+    EEPROM.write(0, Sun_rise_hour);
+    EEPROM.write(1, Sun_rise_minute);
+    EEPROM.write(2, Sun_set_hour);
+    EEPROM.write(3, Sun_set_minute);
+    EEPROM.commit();
+  }
+  else if (type == "Light")
+  {
+    Serial.println("Ready to open led");
+    if (Mqtt_Sub["light"] == 1)
+    {
+      digitalWrite(LED, 1);
+      Serial.println("open led");
+    }
+    else
+    {
+      digitalWrite(LED, 0);
+      Serial.println("close led");
+    }
+  }
+  else if (type == "Light")
+  {
+    digitalWrite(LED, 0);
+    // pwm频率
+    analogWriteFreq(10000); // 10KHZ
+    analogWrite(LED, Mqtt_Sub["PWM"]);
+  }
+  else
+  {
+    Serial.println("Nothing");
+  }
+}
+void reconnect()
+{
+  // Loop until we're reconnected
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266-LouYu";
+    // Attempt to connect
+    if (client.connect(clientId.c_str()))
+    {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      // client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("EspIn");
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+ //定时发布信息
+void Pub_msg()
+{
+  String A = "{\"Temp\":";
+  String B = ",\"light\":";
+  String ALL = A + get_temp() + B + digitalRead(LED) + "}";
+  char _ALL[800];
+  ALL.toCharArray(_ALL, 800);
+  client.publish("EspOut", _ALL);
 }
